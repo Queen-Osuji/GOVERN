0d7c26d143d7a719ea9646f0f363aaa60598d0d8
praise-cloud <georgewillpraise1@gmail.com>
2025-06-23 16:16:04 +0100

    updated the page with the neccessary copy

--- src/pages/EbookLanding.jsx
+++ src/pages/EbookLanding.jsx
@@ -74,9 +74,9 @@ const EbookLanding = () => {
       return;
     }
 
-    console.log('Initiating Paystack payment with email:', email);
+    // console.log('Initiating Paystack payment with email:', email);
     const handler = PaystackPop.setup({
-      key: import.meta.env.VITE_TEST_PAYSTACK_PUBLIC_KEY,
+      key: import.meta.env.VITE_PAYSTACK_PUBLIC_KEY,
       email: email,
       amount: 12750000,
       currency: 'NGN',
