f14be9a4955fd222527cd5c41bd53ad9841fa924
praise-cloud <georgewillpraise1@gmail.com>
2025-07-06 02:37:56 +0100

    edited the path of the clinet images

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,7 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
+  build: {
+    assertsInclude: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif'],
+  }
 })
